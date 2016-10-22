e00d61b4ca7e999cbef92f6e1715b978558a3e4b
Piotr Brzezinski <peb.brzezinski@gmail.com>
2016-10-22 21:00:11 +0200

    import images properly

--- src/js/App.js
+++ src/js/App.js
@@ -3,12 +3,12 @@ import '../assets/styles/App.css';
 import AvatarPicker from './AvatarPicker/AvatarPicker';
 
 const avatars = [
-  { "src": "../assets/images/avatar1.png", "label": "Avatar 1", "id": 1 },
-  { "src": "../assets/images/avatar2.png", "label": "Avatar 2", "id": 2 },
-  { "src": "../assets/images/avatar3.png", "label": "Avatar 3", "id": 3 },
-  { "src": "../assets/images/avatar4.png", "label": "Avatar 4", "id": 4 },
-  { "src": "../assets/images/avatar5.png", "label": "Avatar 5", "id": 5 },
-  { "src": "../assets/images/avatar6.png", "label": "Avatar 6", "id": 6 },
+  { "src": require("../assets/images/avatar1.png"), "label": "Avatar 1", "id": 1 },
+  { "src": require("../assets/images/avatar2.png"), "label": "Avatar 2", "id": 2 },
+  { "src": require("../assets/images/avatar3.png"), "label": "Avatar 3", "id": 3 },
+  { "src": require("../assets/images/avatar4.png"), "label": "Avatar 4", "id": 4 },
+  { "src": require("../assets/images/avatar5.png"), "label": "Avatar 5", "id": 5 },
+  { "src": require("../assets/images/avatar6.png"), "label": "Avatar 6", "id": 6 },
 ];
 
 class App extends Component {
