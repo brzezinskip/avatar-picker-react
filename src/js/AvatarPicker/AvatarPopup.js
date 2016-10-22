ce9002594acb067d514fce2883c2e4c2ce968395
Piotr Brzezinski <peb.brzezinski@gmail.com>
2016-10-22 23:36:06 +0200

    popup animation

--- src/js/AvatarPicker/AvatarPopup.js
+++ src/js/AvatarPicker/AvatarPopup.js
@@ -1,47 +1,11 @@
 import React, { Component } from 'react';
 import Avatar from './Avatar';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import { pulse } from 'react-animations'
 
 import '../../assets/styles/Avatar/AvatarList.css';
 import '../../assets/styles/Avatar/AvatarPopup.css';
 
 export default class AvatarPopup extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state ={
-      isOpenedStyle: {},
-    }
-  }
-
-  componentWillReceiveProps(newProps) {
-    this.animation(newProps);
-  }
-
-  animation(newProps) {
-    let keyframes = `@-webkit-keyframes fadeIn {
-          95% { -webkit-transform:transform(3)) }
-          100% { -webkit-transform:transform(1)) }
-        }`
-    const isOpened = newProps.isOpened ?
-      {
-
-        opacity: 1,
-        transform: 'scale(1)',
-        keyframes,
-      } :
-      {
-        // visibility: 'hidden'
-      };
-
-      const style = document.createElement('style');
-      document.getElementsByClassName('avatar-popup')[0].appendChild(style);
-      style.appendChild(document.createTextNode(keyframes));
-
-      this.setState({
-        isOpenedStyle: isOpened
-      })
-  }
 
   render() {
 
@@ -54,7 +18,7 @@ export default class AvatarPopup extends Component {
       </li>
     );
     return (
-      <div className="avatar-popup" style={ this.state.isOpenedStyle }>
+      <div className={`avatar-popup ${ this.props.isOpened? 'show' : '' }`} style={ pulse }>
         <p className="choose-text"> Choose your avatar </p>
         <ul className="avatar-list">
           { avatars }
