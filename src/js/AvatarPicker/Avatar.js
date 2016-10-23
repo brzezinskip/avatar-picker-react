3a15cab96ca1c3b023ba7fc3087d1517e47e1160
Piotr Brzezinski <peb.brzezinski@gmail.com>
2016-10-23 16:25:31 +0200

    make avatar component stateless

--- src/js/AvatarPicker/Avatar.js
+++ src/js/AvatarPicker/Avatar.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../assets/styles/Avatar/Avatar.css';
 
-export default class Avatar extends Component {
-
-  generateLoadingBorder() {
-    if (! this.props.isMainScreenAvatar && this.props.avatar.isLoading) {
+function generateLoadingBorder(props) {
+  if (! props.isMainScreenAvatar && props.avatar.isLoading) {
       return (
         <div
           className='border-animation'
-          onClick={ () => this.props.onPress() }
+          onClick={ () => props.onPress() }
         />
       );
     }
     return null;
-  };
+}
 
-  render() {
-    const isLoadingBorderStyle = (this.props.avatar.isLoading) ? {
-      border: '3px solid transparent',
-    } : {};
-    const imageStyles = `avatar-image ${ this.props.isMainScreenAvatar ?
-      'main' : 'list' } ${ this.props.avatar.isSelected ? 'selected' : ''} `
-    return (
+export default function Avatar(props) {
+
+  const isLoadingBorderStyle = (props.avatar.isLoading) ? {
+    border: '3px solid transparent',
+  } : {};
+  const imageStyles = `avatar-image ${ props.isMainScreenAvatar ?
+    'main' : 'list' } ${ props.avatar.isSelected ? 'selected' : ''} `
+
+  return (
+    <div
+      className={ `avatar ${ props.isMainScreenAvatar ? 'main-avatar' : 'list-avatar' }` }
+    >
+      { generateLoadingBorder(props) }
       <div
-        className={ `avatar ${ this.props.isMainScreenAvatar ? 'main-avatar' : 'list-avatar' }` }
+        className={ `${ (props.isMainScreenAvatar) ? '' : 'overlay' } ${ props.avatar.isSelected ? 'selected': '' }` }
+        style={ isLoadingBorderStyle }
       >
-        { this.generateLoadingBorder() }
-        <div
-          className={ `${ (this.props.isMainScreenAvatar) ? '' : 'overlay' } ${ this.props.avatar.isSelected ? 'selected': '' }` }
-          style={ isLoadingBorderStyle }
-        >
-          <img
-            className={ imageStyles }
-            src={ this.props.avatar.src }
-            alt='avatar'
-            onClick={ () => this.props.onPress(this.props.avatar) }
-          />
-        </div>
+        <img
+          className={ imageStyles }
+          src={ props.avatar.src }
+          alt='avatar'
+          onClick={ () => props.onPress(props.avatar) }
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
